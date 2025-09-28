64f93448551c9748103471e5779809ac9ec485ac
Benjamin Bencosme <benjaminbencosme237@gmail.com>
2025-09-28 11:57:56 -0400

    Haciendo LESSON 1-COMPLETE

---
+++ LESSON 1/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    // 1. Usar lateinit para extraer la variable de vista de imagen
+    private lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+        // 2. Inicializar la variable de vista de imagen
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        val diceRoll = (1..6).random()
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+    }
+}
